b9f5a9f8810466947d9df33090ae2e97d6826a63
Kurumi-Tokito <RustedSilver11@proton.me>
2025-06-01 17:17:30 +0530

    Revert "treewide: introduce E404 kernel signature build"
    - for now disable E404 kernel attributes
    
    This reverts commit bb3bdda000d83d2e6c4634a623a61368ede224d2.
    This reverts commit 5d6bd428a562a756600e93f1e44e65b295be14f3.

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -17,10 +17,6 @@
 #include <linux/sched.h>
 #include <linux/cpu_cooling.h>
 
-#ifdef CONFIG_E404_SIGNATURE
-#include <linux/e404_attributes.h>
-#endif
-
 #define CREATE_TRACE_POINTS
 #include <trace/events/dcvsh.h>
 
@@ -500,7 +496,7 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 	unsigned long cpu;
 	int ret, of_len, max_index;
 	u32 *of_table = NULL;
-	char tbl_name[32];
+	char tbl_name[] = "qcom,cpufreq-table-##";
 	bool invalidate_freq;
 
 	c->table = devm_kcalloc(dev, lut_max_entries + 1,
@@ -508,19 +504,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 	if (!c->table)
 		return -ENOMEM;
 
-#ifdef CONFIG_E404_SIGNATURE
-	if (e404_data.e404_effcpu == 1) {
-		snprintf(tbl_name, sizeof(tbl_name), "qcom,effcpufreq-table-%d", domain_index);
-		pr_alert("E404: Using effcpu CPUFreq from DTB");
-	} else {
-		snprintf(tbl_name, sizeof(tbl_name), "qcom,cpufreq-table-%d", domain_index);
-		pr_alert("E404: Using normal CPUFreq from DTB");
-	}
-#else
-	snprintf(tbl_name, sizeof(tbl_name), "qcom,cpufreq-table-%d", domain_index);
-	pr_alert("E404: Using default CPUFreq from DTB");
-#endif
-
+	snprintf(tbl_name, sizeof(tbl_name), "qcom,cpufreq-table-%d",
+		 domain_index);
 	if (of_find_property(dev->of_node, tbl_name, &of_len) && of_len > 0) {
 		of_len /= sizeof(*of_table);
 
